8cfd9ed8a73d1100580bf7a735fb0cc02129f4d5
pinoChang <z8817176@163.com>
2017-09-22 16:47:39 +0800

    修正系统错误

--- gulpfile.js
+++ gulpfile.js
@@ -55,75 +55,76 @@ gulp.task('render', function() {
 
 /*监听less文件，编译输出src/css目录*/
 var lessCompile = function(lessPath, cssFolder) {
-        return gulp.src(lessPath)
-            .pipe(sourcemaps.init())
-            .pipe(less()).on('error', function(err) {
-                showError(err)
-            })
-            .pipe(autoprefixer([
-                'ie >= 9',
-                'edge >= 20',
-                'ff >= 44',
-                'chrome >= 48',
-                'safari >= 8',
-                'opera >= 35',
-                'ios >= 8'
-            ]))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest(cssFolder))
-            .on('end', function() {
-                browsersync.reload();
-            })
-    }
-    /* 转换less成css */
+    return gulp.src(lessPath)
+        .pipe(sourcemaps.init())
+        .pipe(less(
+        )).on('error', function(err) {
+            showError(err)
+        })
+        .pipe(autoprefixer([
+            'ie >= 9',
+            'edge >= 20',
+            'ff >= 44',
+            'chrome >= 48',
+            'safari >= 8',
+            'opera >= 35',
+            'ios >= 8'
+        ]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(cssFolder))
+        .on('end', function() {
+            browsersync.reload();
+        })
+}
+/* 转换less成css */
 gulp.task('less', function() {
     lessCompile(path.less, path.cssfolder);
 })
 
 /*output dist/css*/
 var cssOut = function(cssPath, cssOutPath) {
-        return gulp.src(cssPath)
-            .pipe(cleancss({
-                compatibility: 'ie8'
-            }))
-            .pipe(gulp.dest(cssOutPath))
-    }
-    /* 转移src下的css到dist，并压缩 */
+    return gulp.src(cssPath)
+        .pipe(cleancss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest(cssOutPath))
+}
+/* 转移src下的css到dist，并压缩 */
 gulp.task('css', ['less'], function() {
     cssOut(path.css, path.cssout)
 })
 
 /*output dist/script*/
 var scriptOut = function(jsPath, jsOutPath) {
-        return gulp.src(jsPath)
-            .pipe(uglify())
-            .pipe(gulp.dest(jsOutPath))
-    }
-    /* 转移src下的script到dist，并压缩 */
+    return gulp.src(jsPath)
+        .pipe(uglify())
+        .pipe(gulp.dest(jsOutPath))
+}
+/* 转移src下的script到dist，并压缩 */
 gulp.task('script', function() {
     scriptOut(path.js, path.jsout)
 })
 
 /* output images*/
 var imagesOut = function(imagePath, imageOutPath) {
-        return gulp.src(imagePath)
-            .pipe(imagemin())
-            .pipe(gulp.dest(imageOutPath))
-    }
-    /* 转移src下的image到dist，并压缩 */
+    return gulp.src(imagePath)
+        .pipe(imagemin())
+        .pipe(gulp.dest(imageOutPath))
+}
+/* 转移src下的image到dist，并压缩 */
 gulp.task('images', function() {
     imagesOut(path.image, path.imageout);
 })
 
 var vendorOut = function(vendorPath, vendorOutPath) {
-        return gulp.src(vendorPath)
-            .pipe(gulp.dest(vendorOutPath))
-    }
-    /* 转移src下的vendor到dist */
+    return gulp.src(vendorPath)
+        .pipe(gulp.dest(vendorOutPath))
+}
+/* 转移src下的vendor到dist */
 gulp.task('vendor', function() {
-        vendorOut(path.vendor, path.vendorout)
-    })
-    /* 转移src下的conf到dist */
+    vendorOut(path.vendor, path.vendorout)
+})
+/* 转移src下的conf到dist */
 gulp.task('conf', function() {
     vendorOut(path.conf, path.confout)
 })
@@ -132,7 +133,7 @@ gulp.task('conf', function() {
 gulp.task('live', ['less', 'render'], function() {
     browsersync.init({
         port: 3333,
-        startPath: './src/html/index.html', //默认打开的初始地址，可以不加
+        startPath:'./src/html/index.html',//默认打开的初始地址，可以不加
         server: {
             baseDir: './',
             directory: true
@@ -144,7 +145,7 @@ gulp.task('live', ['less', 'render'], function() {
 })
 
 gulp.task('output', ['html', 'css', 'script', 'images', 'vendor', 'conf'])
-    //最终产出
+//最终产出
 gulp.task('dist', ['output'])
 
 //启动服务
